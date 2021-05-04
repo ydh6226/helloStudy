ab81e55c45333434a24a8c3d9f0e998117c7f501
ydh6226 <ydh6226@naver.com>
2021-05-04 20:24:33 +0900

    [ADD] 알림 아이콘 변경 + 알림 Handler Interceptor 등록

--- src/main/java/com/hellostudy/modules/notification/NotificationRepository.java
+++ src/main/java/com/hellostudy/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.hellostudy.modules.notification;
 
+import com.hellostudy.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean b);
 }
